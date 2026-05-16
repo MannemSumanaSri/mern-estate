0e747ce817d65707e7a0a4f02260d66d62b8b4a8
Mannem Sumana Sri <mannemsumanasri@gmail.com>
2026-05-16 19:17:10 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user:userReducer},
-  middleware:(getDefaultMiddleware)=>
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-        serializableCheck: false,
+      serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy : {
-      '/api' : {
-        target:'http://localhost:3000',
-        secure : false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
