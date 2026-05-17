b6cb1f26850c2ff38e79c45ff5412d3f38cf8d77
Mannem Sumana Sri <mannemsumanasri@gmail.com>
2026-05-17 16:14:44 +0530

    add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
@@ -107,7 +108,20 @@ export default function Profile() {
 
     }
   };
-
+const handleSignOut = async() =>{
+  try{
+    dispatch(signOutUserStart())
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if(data.success===false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data.message));
+  }catch(error){
+    dispatch(deleteUserFailure(data.message));
+  }
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
 
@@ -170,7 +184,7 @@ export default function Profile() {
           Delete Account
         </span>
 
-        <span className="text-red-700 cursor-pointer">
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
           Sign Out
         </span>
 
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -49,6 +49,18 @@ const userSlice = createSlice({
     deleteUserFailure:(state,action)=>{
       state.error=action.payload;
       state.loading=false;
+    },
+    signOutUserStart:(state)=>{
+      state.loading=true;
+    },
+    signOutUserSuccess:(state)=>{
+      state.currentUser=null;
+      state.loading=false;
+      state.error=null;
+    },
+    signOutUserFailure:(state,action)=>{
+      state.error=action.payload;
+      state.loading=false;
     }
 
   },
@@ -64,6 +76,9 @@ export const {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
