ccc713c6607603bab3c36cb0013e46c2c2eb6462
Mannem Sumana Sri <mannemsumanasri@gmail.com>
2026-05-17 18:28:07 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from "../redux/user/userSlice";
+import {Link} from 'react-router-dom';
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector(
@@ -157,6 +158,9 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+        Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
