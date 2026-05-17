67d2a99c655f59d04d5433f985f2c8733740a761
Mannem Sumana Sri <mannemsumanasri@gmail.com>
2026-05-17 09:46:21 +0530

    create update user api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
-
 export default function Profile() {
+  
   const {currentUser}=useSelector((state)=>state.user)
   return (
     <div className="p-3 max-w-lg mx-auto">
